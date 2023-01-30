1267373ff6dd1703afd5d17d5faff4eb17a8741e
BOOZYBOSS <123509663+BOOZYBOSS@users.noreply.github.com>
2023-01-30 11:38:29 +0100

    Delete script1.js

--- js/script1.js
+++
@@ -1,7 +0,0 @@
-let day = document.querySelector(".day").innerText;
-let howMuch = document.querySelector(".howMuch");
-
-day === "poniedziałek"
-    ? console.log("Mamy poniedziałek")
-    : howMuch.remove();
-
